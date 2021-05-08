856af99b53de9890ba008d0bf24ec590a30985ab
Jatin <jatin.dhar27@gmail.com>
2021-05-08 16:35:33 +0530

    arithmetic operation in bash script

---
+++ arithmetic_with_integer.sh
@@ -0,0 +1,17 @@
+read -p "Enter value of num1 " num1
+read -p "Enter vaueo of num2 " num2
+
+echo  "Addition"
+echo $((num1 + num2))
+
+echo "multiplication"
+echo $((num1 * num))
+
+echo "division"
+echo $((num1 / num2))
+
+echo "subtraction"
+echo $((num1 - num2))
+
+echo "modulas"
+echo $((num1 % num2))
